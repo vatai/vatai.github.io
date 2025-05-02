f6e7682a41c924835a5b0d251dbac4d3fc7b1ee1
vatai <emil.vatai@gmail.com>
2025-05-02 08:16:31 +0000

    Deploying to gh-pages from @ vatai/vatai.github.io@b866c68b05dc003dd8a741da82d0111da441c9f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
